048392b82789b82053864a38403a9f5a142b184e
waqas <wmunir232@gmail.com>
2024-02-04 19:32:05 +0500

    Registeration Route

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 import express from "express";
 
+import authRoutes from "./routes/auth-routes.js";
+import mongoose from "mongoose";
+
 const app = express();
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || "5000";
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
 app.listen(PORT, () => {
-  console.log("Server is running at Port 5000");
+  mongoose
+    .connect(
+      "mongodb+srv://devwaqas232:devwaqas232@blog.leftka8.mongodb.net/blog"
+    )
+    .then(() => {
+      console.log("Server is running at Port 5000");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
---
+++ src/controllers/auth-controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import User from "models/user-model.js";
+import { type UserSchema } from "models/user-model.js";
+
+export const register = async (req: Request, res: Response) => {
+  const data: UserSchema = await req.body;
+  try {
+    const checkUser = await User.findOne({ email: data.email });
+    if (checkUser) {
+      return res
+        .status(409)
+        .json({ message: "User already exists", ok: false });
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 12);
+    const user = await User.create({
+      ...data,
+      password: hashedPassword,
+    });
+    res
+      .status(201)
+      .json({ message: "User Created Successfully", ok: true, user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error", ok: false });
+  }
+};
