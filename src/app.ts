2c1d0f6f18b1de60c3dfece81a5f99fcb1633cfd
waqas <wmunir232@gmail.com>
2024-02-08 16:29:13 +0500

    Completed Authentication

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from "express";
 
 import authRoutes from "./routes/auth-routes.js";
 import mongoose from "mongoose";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
