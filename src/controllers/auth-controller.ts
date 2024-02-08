a4067d9817e975b7f1343a926543e9acecf4bc45
waqas <wmunir232@gmail.com>
2024-02-08 15:58:02 +0500

    Implemented Login Route

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 
 import User from "models/user-model.js";
 import { type UserSchema } from "models/user-model.js";
-import { authenticateJWT, generateToken } from "middlewares/jwt.js";
+import { generateToken } from "middlewares/jwt.js";
 
 export const register = async (req: Request, res: Response) => {
   const data: UserSchema = await req.body;
@@ -29,6 +29,38 @@ export const register = async (req: Request, res: Response) => {
       .json({ message: "User Created Successfully", ok: true, token });
   } catch (error) {
     console.log(error);
+    res.status(500).json({ message: "Server Error", ok: false });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  const data: { email: string; password: string } = req.body;
+  try {
+    const userExists = await User.findOne({ email: data.email });
+    if (!userExists) {
+      return res
+        .status(404)
+        .json({ message: "Invalid Credentials", ok: false });
+    }
+    const passwordMatch = await bcrypt.compare(
+      data.password,
+      userExists.password
+    );
+    if (!passwordMatch) {
+      return res
+        .status(404)
+        .json({ message: "Invalid Credentials", ok: false });
+    }
+
+    const token = generateToken({
+      email: data.email,
+      userId: userExists._id,
+    });
+
+    return res
+      .status(200)
+      .json({ message: "Logged in successfully", ok: true, token });
+  } catch (error) {
     res.status(500).json({ message: "Server Error", ok: false });
   }
 };
